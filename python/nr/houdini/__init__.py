dd77bf6930868412474f645ed5d8943878103495
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-01 00:16:03 +0200

    add nr.houdini and localimport modules to python/ directory, update .gitignore

---
+++ python/nr/houdini/__init__.py
@@ -0,0 +1,63 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+__author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
+__version__ = '1.0.0'
+
+import hou
+import localimport as _localimport
+import os
+import weakref
+
+_importer = None
+_path = None
+
+def localimport(*args, **kwargs):
+  """
+  Get or create a new #localimport<_localimport> object for the current
+  Houdini file. You should keep a reference to the returned object as
+  otherwise it will be lost.
+
+  The returned localimport object will contain as Python module search path
+  the python/ directory in the parent directory of the HIP file.
+
+  # Parameters
+  args: Forwarded to the localimport constructor.
+  kwargs: Forwarded to the localimport constructor.
+  develop (bool): Set to #True to create a new, separate localimport object.
+    Useful during development when you want to always reload modules from
+    the local python/ modules directory.
+  """
+
+  global _importer, _path
+
+  develop = kwargs.pop('develop', False)
+  python_dir = os.path.join(os.path.dirname(hou.hipFile.path()), 'python')
+  if not _importer or python_dir != _path or develop:
+    importer = _localimport.localimport([python_dir], *args, **kwargs)
+    if not develop:
+      _importer = weakref.ref(importer)
+      _path = python_dir
+  else:
+    importer = _importer
+
+  return importer
