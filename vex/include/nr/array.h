1e358f1d610da5c2904d0b9c5eb4a4ac2a74e10a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-13 02:34:16 +0200

    vex: nr/array.h now uses template macros for function definitions, add nr_array_sorted_insert_index() and nr_array_sorted_indexof() functions

--- vex/include/nr/array.h
+++ vex/include/nr/array.h
@@ -1,6 +1,6 @@
 /* Copyright (c) 2018 Niklas Rosenstein
  *
- * last modified: 2018-05-12
+ * last modified: 2018-05-13
  */
 
 /**
@@ -11,12 +11,17 @@
  * @param v: The value to search for.
  * @return: The index of the value or -1.
  */
-int nr_array_indexof(int a[]; int v) {
-  for (int i = 0; i < len(a); ++i) {
-    if (a[i] == v) return i;
-  }
-  return -1;
+#define DEFINE_NR_ARRAY_INDEXOF(TYPE) \
+int nr_array_indexof(TYPE a[]; TYPE v) { \
+  for (int i = 0; i < len(a); ++i) { \
+    if (a[i] == v) return i; \
+  } \
+  return -1; \
 }
+
+DEFINE_NR_ARRAY_INDEXOF(int)
+DEFINE_NR_ARRAY_INDEXOF(string)
+
 
 /**
  * Appends the value *v* to the array *a* only if the value does not already
@@ -26,23 +31,105 @@ int nr_array_indexof(int a[]; int v) {
  * @param v: The value to append to the array.
  * @return: 1 if the value was appended, 0 otherwise.
  */
-int nr_array_unique_append(int a[]; int v) {
-  if (nr_array_indexof(a, v) == -1) {
-    append(a, v);
-    return 1;
-  }
-  return 0;
+#define DEFINE_NR_ARRAY_UNIQUE_APPEND(TYPE) \
+int nr_array_unique_append(TYPE a[]; TYPE v) { \
+  if (nr_array_indexof(a, v) == -1) { \
+    append(a, v); \
+    return 1; \
+  } \
+  return 0; \
 }
+
+DEFINE_NR_ARRAY_UNIQUE_APPEND(int)
+DEFINE_NR_ARRAY_UNIQUE_APPEND(string)
+
 
 /**
  * Returns the common elements of the two arrays *a* and *b*.
  */
-int[] nr_array_common_elements(int a[], b[]) {
-  int result[];
-  for (int i = 0; i < len(a); ++i) {
-    if (nr_array_indexof(b, a[i]) != -1) {
-      append(result, a[i]);
-    }
-  }
-  return result;
+#define DEFINE_NR_ARRAY_COMMON_ELEMENTS(TYPE) \
+TYPE[] nr_array_common_elements(TYPE a[], b[]) { \
+  TYPE result[]; \
+  for (int i = 0; i < len(a); ++i) { \
+    if (nr_array_indexof(b, a[i]) != -1) { \
+      append(result, a[i]); \
+    } \
+  } \
+  return result; \
 }
+
+DEFINE_NR_ARRAY_COMMON_ELEMENTS(int)
+DEFINE_NR_ARRAY_COMMON_ELEMENTS(string)
+
+
+/**
+ * Returns the insert index for *v* into the sorted array *a*.
+ * Using the returned index to insert the value will keep the
+ * array in its sorted state.
+ *
+ * @param a: The array where the value would be inserted into.
+ * @param v: The value that would be inserted.
+ * @param num: The number of elements from the beginning of the
+ *    array to search through. If omitted, defaults to the actual
+ *    size of the array.
+ */
+#define DEFINE_NR_ARRAY_SORTED_INSERT_INDEX(TYPE) \
+int nr_array_sorted_insert_index(TYPE a[]; TYPE v; int num) { \
+  int left = 0, right = num - 1; \
+  while (left <= right) { \
+    int pivot = (left + right) / 2; \
+    if (a[pivot] < v) { \
+      if (left == right) return pivot + 1; \
+      left = pivot + 1; \
+    } \
+    else if (a[pivot] > v) { \
+      if (left == right) return pivot -1; \
+      right = pivot - 1; \
+    } \
+    else { \
+      return pivot; \
+    } \
+  } \
+  return left; \
+} \
+int nr_array_sorted_insert_index(TYPE a[]; TYPE v) { \
+  return nr_array_sorted_insert_index(a, v, len(a)); \
+}
+
+DEFINE_NR_ARRAY_SORTED_INSERT_INDEX(int)
+DEFINE_NR_ARRAY_SORTED_INSERT_INDEX(string)
+
+
+/**
+ * Returns the index of *v* in the sorted array *a*. Returns -1
+ * if the elemnt does not appear in the array.
+ *
+ * @param a: The sorted array to search through.
+ * @param v: The value to search for.
+ * @param num: The number of elements from the beginning of the
+ *    array to search through. If omitted, defaults to the actual
+ *    size of the array.
+ */
+#define DEFINE_NR_ARRAY_SORTED_INDEXOF(TYPE) \
+int nr_array_sorted_indexof(TYPE a[]; TYPE v; int num) { \
+  int left = 0, right = num - 1; \
+  while (left <= right) { \
+    int pivot = (left + right) / 2; \
+    if (a[pivot] < v) { \
+      left = pivot + 1; \
+    } \
+    else if (a[pivot] > v) { \
+      right = pivot - 1; \
+    } \
+    else { \
+      return pivot; \
+    } \
+  } \
+  return -1; \
+}\
+int nr_array_sorted_indexof(TYPE a[]; TYPE v) { \
+  return nr_array_sorted_indexof(a, v, len(a)); \
+} \
+
+DEFINE_NR_ARRAY_SORTED_INDEXOF(int)
+DEFINE_NR_ARRAY_SORTED_INDEXOF(string)
