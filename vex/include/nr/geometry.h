0f17116d0811ebb43ab7c2e64569c7e7244eab67
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-14 19:51:31 +0200

    vex: <nr/geometry.h> -- added nr_geometry_triangle_area() and nr_geometry_local_one_ring_area()

--- vex/include/nr/geometry.h
+++ vex/include/nr/geometry.h
@@ -27,3 +27,32 @@ int[] nr_geometry_connected_points(int geometry, ptnum) {
   }
   return result;
 }
+
+
+/**
+ * Calculates the area of the triangle composed of the specified
+ * points *a*, *b* and *c*.
+ */
+float nr_geometry_triangle_area(vector a, b, c) {
+  float A = length(a - b);
+  float B = length(b - c);
+  float C = length(c - a);
+  float s = (A + B + C);
+  return sqrt(s * (s - A) * (s - B) * (s - C));
+}
+
+
+/**
+ * Calculates the local one ring area of the point *p*.
+ */
+float nr_geometry_local_one_ring_area(int geo, p) {
+  float area = 0.0;
+  foreach (int prim; pointprims(geo, p)) {
+    int pts[] = primpoints(geo, prim);
+    area += nr_geometry_triangle_area(
+        point(geo, 'P', pts[0]),
+        point(geo, 'P', pts[1]),
+        point(geo, 'P', pts[2]));
+  }
+  return area;
+}
