daf8edc335ae2e8a665e2e8e050db8bfd0d727c6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-15 23:15:43 +0200

    vex: <nr/geometry.h> reorder functions

--- vex/include/nr/geometry.h
+++ vex/include/nr/geometry.h
@@ -3,30 +3,8 @@
  * last modified: 2018-05-12
  */
 
+#include <assert.h>
 #include <nr/array.h>
-
-/**
- * Returns an array of the points connected to the point *p*.
- *
- * @param geometry: The geometry to search.
- * @param p: The point number.
- * @return: An array of point numbers.
- */
-int[] nr_geometry_connected_points(int geometry, ptnum) {
-  int result[];
-  int prims[] = pointprims(geoself(), ptnum);
-  for (int i = 0; i < len(prims); ++i) {
-    int points[] = primpoints(geoself(), prims[i]);
-    int index = nr_array_indexof(points, ptnum);
-    if (index >= 0 && len(points) >= 2) {
-      nr_array_unique_append(result, points[(index-1)%len(points)]);
-    }
-    if (len(points) >= 3) {
-      nr_array_unique_append(result, points[(index+1)%len(points)]);
-    }
-  }
-  return result;
-}
 
 
 /**
@@ -44,6 +22,9 @@ float nr_geometry_triangle_area(vector a, b, c) {
 
 /**
  * Calculates the local one ring area of the point *p*.
+ *
+ * @param geo: The geometry.
+ * @param p: The point number to compute the local one ring area of.
  */
 float nr_geometry_local_one_ring_area(int geo, p) {
   float area = 0.0;
@@ -55,6 +36,34 @@ float nr_geometry_local_one_ring_area(int geo, p) {
         point(geo, 'P', pts[2]));
   }
   return area;
+}
+
+
+/**
+ * Returns an array of the points connected to the point *p*.
+ * If the *sorted* argument is true, the returned array of points
+ * represents the one-ring of the point in correct walking order.
+ *
+ * @param geometry: The geometry to search.
+ * @param p: The point number.
+ * @param sorted: Whether the returned array is sorted to obtain
+ *    the proper one-ring point order.
+ * @return: An array of point numbers.
+ */
+int[] nr_geometry_connected_points(int geo, ptnum) {
+  int result[];
+  int prims[] = pointprims(geo, ptnum);
+  for (int i = 0; i < len(prims); ++i) {
+    int points[] = primpoints(geo, prims[i]);
+    int index = nr_array_indexof(points, ptnum);
+    if (index >= 0 && len(points) >= 2) {
+      nr_array_unique_append(result, points[(index-1)%len(points)]);
+    }
+    if (len(points) >= 3) {
+      nr_array_unique_append(result, points[(index+1)%len(points)]);
+    }
+  }
+  return result;
 }
 
 
